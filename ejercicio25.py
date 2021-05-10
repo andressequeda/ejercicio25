791a8d1be246589340ca1e412c935c4535e8664a
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-10 09:30:25 -0500

    Add files via upload

---
+++ ejercicio25.py
@@ -0,0 +1,5 @@
+numero  =  int ( input ( "Ingrese un número:" ))
+si  numero  > =  10 :
+    print ( "el triple del número es" , numero  *  3 )
+otra cosa :
+    print ( "La cuarta parte del número es:" , numero  /  4 )
